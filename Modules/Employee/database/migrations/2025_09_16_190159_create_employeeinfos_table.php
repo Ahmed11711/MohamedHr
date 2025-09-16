407025e86680d5fc588e38b7d10708e5c112cc38
mohammedfawzii <mo.fawzi.abz@gmail.com>
2025-09-16 22:14:57 +0300

    add Employeeseeder in MainSeeder

--- Modules/Employee/database/migrations/2025_09_16_190159_create_employeeinfos_table.php
+++ Modules/Employee/database/migrations/2025_09_16_190159_create_employeeinfos_table.php
@@ -4,15 +4,14 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
     public function up(): void
     {
         Schema::create('employeeinfos', function (Blueprint $table) {
-              $table->id();
+            $table->id();
             $table->foreignId('facility_id')->constrained()->onDelete('cascade');
             $table->boolean('saudi_national')->default(0);
             $table->json('firstName');
