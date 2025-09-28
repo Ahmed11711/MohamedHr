71807a3085232318e51108009a1c113089058a16
mohammedfawzii <mo.fawzi.abz@gmail.com>
2025-09-28 19:27:42 +0300

    delet Recruitment from Compliance branch

--- Modules/Recruitment/routes/web.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-use Modules\Recruitment\Http\Controllers\RecruitmentController;
-
-Route::middleware(['auth', 'verified'])->group(function () {
-    Route::resource('recruitments', RecruitmentController::class)->names('recruitment');
-});
